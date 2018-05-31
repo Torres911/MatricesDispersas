0a7b88948542f3dbf5e74892a4f3aae0384165c2
pangar03 <32374785+pangar03@users.noreply.github.com>
2018-05-31 07:44:47 -0500

    Update ColumnaCompresa.cpp

--- Proyecto/ColumnaCompresa.cpp
+++ Proyecto/ColumnaCompresa.cpp
@@ -96,16 +96,3 @@ void ColumnaCompresa::modificarPosicion(int x, int y, int v){
 		}
 	}
 }
-
-
-int main(){
-	vector< vector<int> > ex {{0,0,4,0},{1,0,0,0},{4,4,4,4},{3,2,1,0},{0,0,0,0}};
-    vector< vector<int> > ex2 {{2,4,0,2,},{3,2,1,3},{0,0,0,0},{1,2,3,4},{3,4,4,4}};
-    ColumnaCompresa c, c1;
-    c.crearMatrizDispersa(ex);
-    c1.crearMatrizDispersa(ex2);
-    c.obtenerMatrizCompleta();
-    c1.obtenerMatrizCompleta();
-	return 0;
-}
-
