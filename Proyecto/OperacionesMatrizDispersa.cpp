6bb17f259f59ca1d05049061fb7385aab2b0fb92
pangar03 <32374785+pangar03@users.noreply.github.com>
2018-05-31 04:06:06 -0500

    Update OperacionesMatrizDispersa.cpp

--- Proyecto/OperacionesMatrizDispersa.cpp
+++ Proyecto/OperacionesMatrizDispersa.cpp
@@ -1,1 +1,29 @@
 #include "OperacionesMatrizDispersa"
+
+bool MatLE::verificarElem(matrizDispersa m1, int v){
+  bool isElem = false;
+  int tmp;
+
+  for(int i = 0; i < m1.filMat; i++){
+  	for(int j = 0; j < m1.colMat; i++){
+  		tmp = m1.obtenerElemento(i, j);
+  		if(tmp == v)
+  			isElem = true;
+  	}
+  }
+  return isElem;
+}
+
+int MatLE::obtenerMayorElem(matrizDispersa m1){
+	int mayorElem;
+	mayorElem = m1.obtenerElemento(0, 0);
+
+	for(int i = 0; i < m1.filMat; i++){
+		for(int j = 0; j < m1.colMat; i++){
+			if(m1.obtenerElemento(i, j) < mayorElem)
+				mayorElem = m1.obtenerElemento(i, j);
+		}
+	}
+
+	return mayorElem;  
+}
