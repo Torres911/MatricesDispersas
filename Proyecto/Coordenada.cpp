89c3b9b77084aae42fce8c3cae19a0df9833b1d8
Torres911 <33793507+Torres911@users.noreply.github.com>
2018-05-30 23:26:20 -0500

    Cambios

--- Proyecto/Coordenada.cpp
+++ Proyecto/Coordenada.cpp
@@ -4,3 +4,34 @@ void Coordenada::crearMatrizDispersa(vector< vector<int> > &mat){
 
 }
 
+vector< vector<int> > Coordenada::obtenerMatrizCompleta(){
+
+}
+
+vector<int> Coordenada::obtenerFila(int x){
+
+}
+
+vector<int> Coordenada::obtenerColumna(int x){
+
+}
+
+vector<int> Coordenada::obtenerFilaDispersa(int x){
+
+}
+
+vector<int> Coordenada::obtenerColumnaDispersa(int x){
+
+}
+
+int Coordenada::obtenerNumeroElementos(){
+
+}
+
+int Coordenada::obtenerElemento(int x, int y){
+
+}
+
+void Coordenada::modificarPosicion(int x, int y, int v){
+	
+}
